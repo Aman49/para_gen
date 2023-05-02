baceec7d3ddbf6dc3ee3716bbc32e344af276d0f
Aman kumar <amanshrivast49@gmail.com>
2023-05-02 22:39:05 +0530

    First

---
+++ script.js
@@ -0,0 +1,20 @@
+const input = document.getElementById('numberOfWords');
+
+let numOfWords;
+let container = document.querySelector('.container');
+const generateWord = (n) => {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let text = "";
+    for(let i=0; i<n; i++){
+        const random = (Math.random()*(alphabet.length-1)).toFixed(0);
+        text += alphabet[random];
+    }
+    return text;
+}
+const getData = () => {
+    numOfWords = Number(input.value);
+    const para = document.createElement('p');
+    para.innerText = 'sample';
+    para.setAttribute('class', 'paras')
+    container.append(para)
+}
